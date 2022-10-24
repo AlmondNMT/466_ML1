45b4d71e92c43260cf97f5225746675ddaddfa9f
Taylor Grover <tgrover@cc.nmt.edu>
2022-10-23 19:41:54 -0600

    cropping

--- box/script.py
+++ box/script.py
@@ -6,8 +6,13 @@ np.random.seed(1)
 def get_crop(img:Image, width:int, height:int):
     """
     """
+    assert 2 <= width <= 4 and 2 <= height <= 4
+    l = (4 - width) * 200
+    t = (4 - height) * 200
+    new = img.crop((l, t, 1200 - l, 1200 - t))
+    return new
 
-img = Image.open("box.png")
-min_crop = 30
-max_crop = 200 - min_crop
+img = Image.open("6x6.png")
+new = get_crop(img, 3, 2)
+new.show()
 
